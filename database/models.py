3382995d8dbe177d660bc338a71467a3b023d241
1andonlyziz <73785385+1andonlyziz@users.noreply.github.com>
2021-05-29 14:41:56 +0300

    .

---
+++ database/models.py
@@ -0,0 +1,97 @@
+import os
+from sqlalchemy import Column, String, Integer,Date ,create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_name = "capstone"
+database_path = "postgres://{}/{}".format(
+    'postgres:1234@localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+def setup_db(app, database_path=database_path):
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Movies
+Movies with attributes title and release date
+
+'''
+
+class Movie(db.Model):
+
+    id = Column(Integer,primary_key=True)
+    title = Column(String)
+    release_date = Column(Date)
+
+    def __init__(self, title,release_date):
+
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date,
+        }
+
+
+'''
+Actors
+Actors with attributes name, age and gender
+
+'''
+
+class Actor(db.Model):
+
+    id=Column(Integer,primary_key=True)
+    name=Column(String)
+    age=Column(Integer)
+
+
+    def __init__(self, name,age):
+
+        self.name = name
+        self.age = age
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+        }
